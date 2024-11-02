dbac1c8b860647d37b6287cc1ac4b785cc8757c0
alinasser2 <alinasserelden@hotmail.com>
2024-11-02 10:45:52 +0200

    add migration for creating Books table with necessary fields

---
+++ migrations/20241102071728-create-books.js
@@ -0,0 +1,51 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Books', {
+      id: {
+        type: Sequelize.STRING,
+        primaryKey: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      author: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      ISBN: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      quantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      location: {
+        type: Sequelize.STRING,
+      },
+      created_by: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Books');
+  }
+};
