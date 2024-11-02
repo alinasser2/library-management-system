2c3dd448c9708d4439dcea62936df98c92e0f36d
alinasser2 <alinasserelden@hotmail.com>
2024-11-02 10:48:11 +0200

    add migration for creating Borrows table

---
+++ migrations/20241102071745-create-borrows.js
@@ -0,0 +1,57 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Borrows', {
+      id: {
+        type: Sequelize.STRING,
+        primaryKey: true,
+      },
+      book_id: {
+        type: Sequelize.STRING,
+        references: {
+          model: 'Books',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      borrower_id: {
+        type: Sequelize.STRING,
+        references: {
+          model: 'Borrowers',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      borrow_date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+      due_date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      return_date: {
+        type: Sequelize.DATE,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Borrows');
+  }
+};
